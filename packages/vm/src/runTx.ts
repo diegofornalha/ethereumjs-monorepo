233af848d22304161359269eca9936f5dd325c7d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-04-07 15:39:40 -0400

    Make skipBalance adjust just + balance

--- packages/vm/src/runTx.ts
+++ packages/vm/src/runTx.ts
@@ -316,7 +316,7 @@ async function _runTx(this: VM, opts: RunTxOpts): Promise<RunTxResult> {
   const cost = tx.getUpfrontCost(block.header.baseFeePerGas)
   if (opts.skipBalance) {
     // when skipBalance, ensure the caller has enough balance for the total tx cost
-    fromAccount.balance += cost - balance
+    fromAccount.balance += cost
     await this.stateManager.putAccount(caller, fromAccount)
   } else {
     if (balance < cost) {
