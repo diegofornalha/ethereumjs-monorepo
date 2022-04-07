5e20b48e3eb3a95b6c00996bdcc69b33c95fcb06
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-04-07 15:12:18 -0400

    Move skipbalance check to top

--- packages/vm/src/runTx.ts
+++ packages/vm/src/runTx.ts
@@ -314,7 +314,11 @@ async function _runTx(this: VM, opts: RunTxOpts): Promise<RunTxResult> {
   }
 
   const cost = tx.getUpfrontCost(block.header.baseFeePerGas)
-  if (!opts.skipBalance) {
+  if (opts.skipBalance) {
+    // when skipBalance, ensure the caller has enough balance for the total tx cost
+    fromAccount.balance += cost - balance
+    await this.stateManager.putAccount(caller, fromAccount)
+  } else {
     if (balance < cost) {
       const msg = _errorMsg(
         `sender doesn't have enough funds to send tx. The upfront cost is: ${cost} and the sender's account (${caller}) only has: ${balance}`,
@@ -340,9 +344,6 @@ async function _runTx(this: VM, opts: RunTxOpts): Promise<RunTxResult> {
         throw new Error(msg)
       }
     }
-  } else {
-    fromAccount.balance += cost - balance
-    await this.stateManager.putAccount(caller, fromAccount)
   }
   if (!opts.skipNonce) {
     if (nonce !== tx.nonce) {
